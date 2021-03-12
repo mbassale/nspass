e4e41d7d2a2c5c750c7aef0cd57c22dbda0a7bf9
Marco Bassaletti <bassaletti@gmail.com>
2021-03-12 12:43:58 -0300

    Use cryptographically secure random generator to create the initialization vector.

--- src/crypto/InitializationVector.cpp
+++ src/crypto/InitializationVector.cpp
@@ -2,9 +2,8 @@
 // Created by Marco Bassaletti on 12-03-21.
 //
 
-#include <algorithm>
+#include <gcrypt.h>
 #include <vector>
-#include <random>
 #include "InitializationVector.h"
 
 namespace OwnPass::Crypto {
@@ -13,10 +12,7 @@ namespace OwnPass::Crypto {
 	{
 		std::vector<uint8_t> init_vector;
 		init_vector.resize(block_size, 0);
-		std::random_device device;
-		std::mt19937 generator(device());
-		std::uniform_int_distribution<uint8_t> distribution(0, 255);
-		std::generate(init_vector.begin(), init_vector.end(), [&]{ return distribution(generator); });
+		gcry_create_nonce(init_vector.data(), init_vector.size());
 		return init_vector;
 	}
 }
