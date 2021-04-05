970563eab985f612a6de79b7c668bb2f9e0b4eae
Marco Bassaletti <bassaletti@gmail.com>
2021-04-05 16:34:47 -0400

    The console application has a default storage on Application Support directory.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <stdexcept>
 #include <boost/log/trivial.hpp>
+#include "Application.h"
 #include "./cli/CommandLine.h"
 #include "./cli/input/PromptSecretInput.h"
 #include "./cli/input/PromptConfirmInput.h"
 
+using NSPass::Application;
 using NSPass::CLI::CommandLine;
 using NSPass::CLI::Input::PromptSecretInput;
 using NSPass::CLI::Input::PromptConfirmInput;
@@ -11,6 +13,10 @@ using NSPass::CLI::Input::PromptConfirmInput;
 int main(int argc, char* argv[])
 {
 	try {
+		auto& app = Application::instance();
+		app.register_init([&]{
+			app.get_vault().set_storage_location(app.get_settings().get_data_directory() + "/nspass.db");
+		});
 		PromptSecretInput prompt_secret_input{};
 		PromptConfirmInput prompt_confirm_input{};
 		CommandLine command_line{ argc, argv, prompt_secret_input, prompt_confirm_input };
